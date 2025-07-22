16c1556c6975889dce9e9cec25abe3ca504c0595
Martin Ivanov <mivanov@progress.com>
2025-07-22 13:08:56 +0300

    Added WPF sample project

---
+++ src/WpfApplication/WpfApplication/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using Telerik.Windows.Controls;
+
+namespace WpfApplication;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        StyleManager.ApplicationTheme = new Windows11Theme();
+        InitializeComponent();
+
+        var source = new ObservableCollection<RowInfo>();
+        for (int i = 0; i < 100; i++)
+        {
+            source.Add(new RowInfo() { Id = i, Name = $"Record {i}" });
+        }
+        DataContext = source;
+    }
+}
---
+++ src/WpfApplication/WpfApplication/RowInfo.cs
@@ -0,0 +1,7 @@
+﻿namespace WpfApplication;
+
+public class RowInfo
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
